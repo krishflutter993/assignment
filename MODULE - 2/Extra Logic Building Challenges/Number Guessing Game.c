97a8ac6b63484d757797bddeb93a31ed2bd65641
krishflutter993 <savaliyakrish993@gmail.com>
2025-09-19 10:02:42 +0530

    commit

--- MODULE - 2/Extra Logic Building Challenges/Number Guessing Game.c
+++ MODULE - 2/Extra Logic Building Challenges/Number Guessing Game.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main()
 {
-    int secretNumber, guess, attempts = 0;
-    int maxAttempts = 7;
+    int secretNumber = 51;
+    int guess = 0;
 
-    
-    srand(time(0));
-    secretNumber = rand() % 100 + 1; 
+    printf("Guess a number between 1 and 100.\n");
 
-    printf("🎮 Welcome to the Number Guessing Game!\n");
-    printf("Guess the number between 1 and 100. You have %d attempts.\n\n", maxAttempts);
-
-    while (attempts < maxAttempts)
+    while (guess != secretNumber)
     {
         printf("Enter your guess: ");
         scanf("%d", &guess);
 
-        attempts++;
-
-        if (guess == secretNumber)
+        if (guess < 1 || guess > 100)
         {
-            printf("🎉 Congratulations! You guessed the number in %d attempt(s).\n", attempts);
-            break;
+            printf("Please enter a number between 1 and 100.\n");
+            continue;
         }
-        else if (guess < secretNumber)
+
+        if (guess < secretNumber)
         {
             printf("Too low! Try again.\n");
         }
-        else
+        else if (guess > secretNumber)
         {
             printf("Too high! Try again.\n");
         }
-
-        if (attempts == maxAttempts)
+        else
         {
-            printf("❌ You've used all %d attempts. The number was: %d\n", maxAttempts, secretNumber);
+            printf("Congratulations! You guessed the number.\n");
         }
     }
 
