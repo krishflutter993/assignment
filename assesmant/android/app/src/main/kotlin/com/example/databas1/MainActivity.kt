067eefcd17a2d3712de36594ec41f4da90b7c92b
krishflutter993 <savaliyakrish993@gmail.com>
2025-11-14 13:35:50 +0530

    c0mmit

--- assesmant/android/app/src/main/kotlin/com/example/databas1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.databas1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- assesmant/lib/utils/validators.dart
+++
@@ -1,9 +0,0 @@
-class Validators {
-  static bool isValidEmail(String email) {
-    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
-  }
-
-  static bool isValidPassword(String password) {
-    return RegExp(r'^(?=.*[A-Z])(?=.*\d).{6,}$').hasMatch(password);
-  }
-}
